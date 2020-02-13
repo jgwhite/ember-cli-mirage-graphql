f3d4ede2cca3c2770d7cf9897029b0ed5bbe6a56
Jamie White <jameswhite@salesforce.com>
2020-02-13 21:34:12 +0100

    Drop support for Ember < 3.4
    This commit is, I’ll confess, controversial. However, ember-apollo-client
    only supports Ember >= 3.4 and it makes our lives *much* easier to follow
    suit.
    
    The real source of the 3.4 minimum is ember-native-class-polyfill which
    ember-apollo-client now requires Ember < 3.6.
    
    As per [the ember-apollo-client 2.0.0 release notes][1]:
    
    > If you are using Ember versions 3.4 or 3.5 you must add
    > ember-native-class-polyfill to your application.
    
    [1]: https://github.com/ember-graphql/ember-apollo-client/releases/tag/v2.0.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,36 +7,10 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-2.16',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-apollo-client': '2.0.0-beta.1',
-            'ember-source': '~2.16.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-2.18',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-apollo-client': '2.0.0-beta.1',
-            'ember-source': '~2.18.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.4',
         npm: {
           devDependencies: {
-            'ember-apollo-client': '2.0.0-beta.1',
+            'ember-apollo-client': '2.0.0',
             'ember-source': '~3.4.0'
           }
         }
@@ -83,7 +57,7 @@ module.exports = async function() {
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1',
-            'ember-apollo-client': '2.0.0-beta.1'
+            'ember-apollo-client': '2.0.0'
           }
         }
       },
@@ -97,7 +71,7 @@ module.exports = async function() {
         npm: {
           devDependencies: {
             '@ember/jquery': '^0.5.1',
-            'ember-apollo-client': '2.0.0-beta.1'
+            'ember-apollo-client': '2.0.0'
           }
         }
       }
